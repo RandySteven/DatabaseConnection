c08d0d4bf72717c85db807bbf206d4a0ab39e524
Randy Steven <randysteven12@gmail.com>
2020-09-13 09:59:43 +0700

    Database JDBC

---
+++ src/sqlConnection.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class sqlConnection {
+
+	public static Connection dbConnection() {
+//		Connection con = null;
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			con = DriverManager.getConnection("jdbc:mysql://localhost/<DBNAME>", "<USERNAME>", "<PASSWORD>");
+//			System.out.println("Success connect ...");
+//			return con;
+//		} catch (Exception e) {
+//			System.out.println("Fail connect error : " + e);
+//			return con;
+//		}
+		
+		/*
+		 * Create
+		 * By Randy Steven 2020
+		 * End
+		 */
+	}
+}
